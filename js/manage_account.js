2e6820a44ba9663441d0957c6949340a1b961989
diepan-mindx <diepau1312@mindx.net.vn>
2025-02-19 20:01:15 +0700

    completed

---
+++ js/manage_account.js
@@ -0,0 +1,14 @@
+// tao bien luu tru 1 account co dinh
+const defaultAccount = {
+  username: "abc123",
+  email: "abc123@gmail.com",
+  password: "abc123",
+};
+
+// luu bien co dinh vao local storage
+if (!localStorage.getItem("defaultAccount")) {
+  // neu chua co du lieu trong storage => tao
+  // JSON (chuoi - string): kieu du lieu dung de luu vao trong storage => js khong doc duoc
+  // => chuyen kieu du lieu tu js -> json = func stringify()
+  localStorage.setItem("defaultAccount", JSON.stringify(defaultAccount));
+}
